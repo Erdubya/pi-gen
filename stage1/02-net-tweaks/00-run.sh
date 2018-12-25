76f80abc0bb2de633ae10f789a4debefb45f6883
Erik Wilson <erikwilson@outlook.com>
2018-12-25 17:02:42 -0500

    Set custom hostname

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -5,3 +5,4 @@ install -m 644 files/hostname "${ROOTFS_DIR}/etc/hostname"
 echo ${HOSTNAME} > "${ROOTFS_DIR}/etc/hostname"
 
 ln -sf /dev/null "${ROOTFS_DIR}/etc/systemd/network/99-default.link"
+sed -i "s/raspberrypi/${HOSTNAME}/g" "${ROOTFS_DIR}/etc/hosts"
